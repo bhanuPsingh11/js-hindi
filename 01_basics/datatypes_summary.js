87b1b329854ca8aaa41e1d94385ddf4ce3d9b97f
Bhanu Pratap Singh <119893097+bhanuPsingh11@users.noreply.github.com>
2023-09-27 14:45:48 +0000

    stack and heap

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -1,3 +1,7 @@
-const bigNumer=5651651651621215n
+const number = 100;
+const bigNumer=5651651651621215n;
+const accountid = Symbol('1223');
+const name = "rahul";
 
-console.log(typeof bigNumer);
+
+console.table([typeof number,typeof bigNumer,typeof Symbol,typeof name])
---
+++ 01_basics/stack_heap.js
@@ -0,0 +1,24 @@
+let customerName= "rahul"
+let customerEmail="rahul@google.com"
+
+customerName=customerEmail
+customerEmail="plus@google.com"
+console.log(customerName);
+console.log(customerEmail);
+
+
+let userOne = {
+
+    userName:"rahul",
+    upi:"rahul@ybl",
+
+}
+
+let userTwo = userOne
+
+userTwo.upi="rohit@gmail.com"
+
+console.log(userOne.upi);
+console.log(userTwo.upi);
+
+
